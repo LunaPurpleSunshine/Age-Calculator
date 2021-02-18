08b06720e6d58d15261eabffd2cb2fa0a971c8fc
LunaSkySound <69199502+LunaSkyAce@users.noreply.github.com>
2021-02-18 00:11:10 +0000

    Add age at next birthday to output

--- age.py
+++ age.py
@@ -2,24 +2,35 @@ import argparse
 from datetime import date
 
 
-def calculate_age(birthDate): 
+def calculate_age(birthDate):
     today = date.today()
-    age = today.year - birthDate.year - ((today.month, today.day) < (birthDate.month, birthDate.day)) 
-  
+    age = today.year - birthDate.year - ((today.month, today.day) < (birthDate.month, birthDate.day))
+
     return age
+
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
 
-    parser .add_argument("year", nargs=1, help="Required. The birth year of the person to be aged. YYYY or YY (assumes 19XX)")
-    parser .add_argument("month", nargs="?", default="01", const="01", help="Optional (Default 01). The birth month of the person to be aged. MM")
-    parser .add_argument("day", nargs="?", default="01", const="01", help="Optional (Default 01). The birth day of the person to be aged. DD")
+    parser.add_argument("year",
+                        nargs=1,
+                        help="Required. The birth year of the person to be aged. YYYY or YY (assumes 19XX)")
+    parser.add_argument("month",
+                        nargs="?",
+                        default="01",
+                        const="01",
+                        help="Optional (Default 01). The birth month of the person to be aged. MM")
+    parser.add_argument("day",
+                        nargs="?",
+                        default="01",
+                        const="01",
+                        help="Optional (Default 01). The birth day of the person to be aged. DD")
 
     args = parser.parse_args()
 
     year = args.year[0]
-    
+
     if len(year) == 2:
         year = int("19" + year)
     elif len(year) == 4:
@@ -35,7 +46,7 @@ if __name__ == "__main__":
     age = calculate_age(birthDate=birthday)
 
     if age >= 0:
-        print(f"{age} years old")
+        print(f"{age} years old today {date.today()}")
+        print(f"Will turn {age + 1} on next birthday")
     else:
         print(f"{abs(age)} years in the future")
-
