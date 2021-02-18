fcabb8553855a8a41cfede46c35d5a9895efc589
LunaSkySound <69199502+LunaSkyAce@users.noreply.github.com>
2021-02-18 00:51:57 +0000

    Add calulation of when the next birthday is

--- age.py
+++ age.py
@@ -2,11 +2,30 @@ import argparse
 from datetime import date
 
 
-def calculate_age(birthDate):
+def calculate_age(birthDate: date) -> int:
+    "Returns int age based on given birth date."
     today = date.today()
     age = today.year - birthDate.year - ((today.month, today.day) < (birthDate.month, birthDate.day))
 
     return age
+
+
+def calculate_next_birthday(birthDate: date) -> date:
+    "Returns the date of the next birthday, or today's date if it is today"
+    today = date.today()
+    current_year = today.year
+    birth_month = birthDate.month
+    birth_day = birthDate.day
+    current_year_bday = date(current_year, birth_month, birth_day)
+
+    if current_year_bday < today:  # This year's birthday has already passed
+        next = date(current_year + 1, birth_month, birth_day)
+    elif current_year_bday == today:  # Birthday today!
+        next = today
+    else:  # This year's birthday is still to come
+        next = current_year_bday
+
+    return next
 
 
 if __name__ == "__main__":
@@ -44,9 +63,14 @@ if __name__ == "__main__":
     birthday = date(int(year), month, day)
 
     age = calculate_age(birthDate=birthday)
+    next_birthday = calculate_next_birthday(birthDate=birthday)
 
     if age >= 0:
         print(f"{age} years old today {date.today()}")
-        print(f"Will turn {age + 1} on next birthday")
     else:
         print(f"{abs(age)} years in the future")
+
+    if next_birthday == date.today():
+        print(f"Turned {age} today!")
+    else:
+        print(f"Will turn {age + 1} on next birthday {next_birthday}")
